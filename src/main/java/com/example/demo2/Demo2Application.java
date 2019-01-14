dd78e65f6004e1bd7d79b4f66285a7acf567db79
huzai <huzaichuangtianya@163.com>
2019-01-14 17:53:45 +0800

    update

---
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -0,0 +1,28 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class Demo2Application  extends SpringBootServletInitializer{
+	
+
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	// TODO Auto-generated method stub
+	return builder.sources(Demo2Application.class);
+}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+}
+
