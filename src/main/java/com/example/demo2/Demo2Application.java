c62e914c44f1c862b8650ddc0bee3f20dfc37242
huzai <huzaichuangtianya@163.com>
2019-01-17 10:16:28 +0800

    update

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -1,5 +1,6 @@
 package com.example.demo2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@MapperScan("com.example.demo2.mapper")
 public class Demo2Application  extends SpringBootServletInitializer{
 	
 
--- src/main/java/com/example/demo2/controller/HelloController.java
+++ src/main/java/com/example/demo2/controller/HelloController.java
@@ -1,19 +1,48 @@
 package com.example.demo2.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo2.bean.User;
+import com.example.demo2.mapper.UserMapper;
+
 @RestController
 public class HelloController {
+
+    @Autowired
+    private UserMapper userMapper;
 	
-	
-	 @RequestMapping("/")
-	    public String index() {
-	        return "Hello Spring Boot 2.0!";
-	    }
-	 
-	 @RequestMapping("/index")
-	    public String index1() {
-	        return "index";
-	    }
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello Spring Boot 2.0!";
+	}
+
+	@RequestMapping("/index1")
+	public String index1() {
+		return "index";
+	}
+
+	@RequestMapping("/query")
+	public List<Map<String, Object>> query() {
+
+		List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from user");
+		return list;
+	}
+
+	@RequestMapping("/query1")
+	public List<User> query1() {
+		List<User> users = userMapper.getAll();
+        System.out.println("kan:"+users.toString());
+        
+//		List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from user");
+		return users;
+	}
 }
